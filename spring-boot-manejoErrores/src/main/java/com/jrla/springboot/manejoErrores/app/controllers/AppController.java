d589095892cee35d8b9cdea656ff1e4fb6432132
seRRaloR <serralor@gmail.com>
2022-12-15 08:32:25 +0100

    Se capturan todos los errores 500, incluso división por cero

--- spring-boot-manejoErrores/src/main/java/com/jrla/springboot/manejoErrores/app/controllers/AppController.java
+++ spring-boot-manejoErrores/src/main/java/com/jrla/springboot/manejoErrores/app/controllers/AppController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 	
+	
 	@GetMapping("/index")
 	public String index(Model modelo) {
 		
-		
+		@SuppressWarnings("unused")
+		Integer valor = 100/0;
 		return "index";
 	}
 
